c846e2c45f0a47650d374de03e2deeed60e4968c
Emmanouil Richman <erichman966@gmail.com>
2020-11-24 22:14:29 -0500

    Now user can enter desired city

--- src/Views/GetInfo.js
+++ src/Views/GetInfo.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function GetInfo() {
+
+const [city, setCity] = useState("");
+
+
+    function handleSubmit(){
+
+    }
     return (
         <div>
-            <h1>Please Insert Your Current City</h1>
+            <h1>Please Enter The Closest City To You</h1>
+            <form onSubmit={handleSubmit}>
+               <label>City: </label>
+               <input type="text" value={city} onChange={e => setCity(e.target.value)} required />
+               <br></br>
+                <button type="submit" value="Submit">Submit</button>
+            </form>
         </div>
     )
 }
