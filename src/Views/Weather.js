e58afc7a212a1c595cd34dd2748e7d1fabfdddfd
Emmanouil Richman <erichman966@gmail.com>
2020-11-25 22:45:54 -0500

    Now can get 3 day forecast  by city or zip

--- src/Views/Weather.js
+++ src/Views/Weather.js
@@ -1,13 +1,43 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 
 
-var correctName = true;
-
+const KEY = "d7908f4f11ef423aa9854244202511";
 export default function Weather(props) {
+    const[name, setName] = useState('');
+    const[region, setRegion] = useState('');
+    const[country, setCountry] = useState('');
+    const[forecast, setForecast] = useState([]);
+    const URL = `http://api.weatherapi.com/v1/forecast.json?key=${KEY}&q=${props.location.props.location}&days=3`
+
+     useEffect(() => {
+        const fetch = async () =>{
+            const res = await axios.get(
+                URL
+            );
+            setName(res.data.location.name)
+            setRegion(res.data.location.region)
+            setCountry(res.data.location.country)
+            setForecast(res.data.forecast.forecastday)
+        }
+        fetch();
+    }, [URL]);
+    // console.log(forecast[0].date);
     return (
         <div>
-            <h1>5 Day Forcast for {props.location.props.location}</h1>
+            <h1>3 Day forecast for {name}, {region}, {country}</h1>
+
+                {forecast.map(day =>{
+                   return( 
+                   <div>
+                    <img src={day.day.condition.icon} alt="conditions"></img>
+                    <h3>{day.date}</h3>
+                    <p>{day.day.avgtemp_f}</p>
+                   <p>{day.day.condition.text}</p>
+                   </div>
+                   );
+                })}
+        
         </div>
     )
 }
